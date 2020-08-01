377f92a8e8fca6e548004452e58844bdbee85ff3
Cameron Baney <cbaney@gmail.com>
2020-08-01 15:01:59 -0400

    Responsive heading styles

--- components/SiteHeader/SiteHeader.vue
+++ components/SiteHeader/SiteHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="site-header container mx-auto max-w-4xl py-12">
-    <ul class="text-xl text-green-400 flex flex-row justify-between content-center">
+  <div class="site-header container mx-auto max-w-4xl py-6 sm:py-12">
+    <ul class="text-xl text-green-400 flex flex-col sm:flex-row sm:justify-between sm:content-center">
       <li><nuxt-link to="/">Home</nuxt-link></li>
       <li><nuxt-link to="experience">Experience</nuxt-link></li>
       <li><nuxt-link to="education">Education</nuxt-link></li>
