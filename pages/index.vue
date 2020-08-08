68089cd2eb60523fdaee42fbc3c5a371c505997c
Cameron Baney <cbaney@gmail.com>
2020-08-08 17:09:10 -0400

    Responsive homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h1 class="text-5xl font-bold mb-0">Cameron Baney</h1>
-    <h2 class="text-3xl font-semibold mb-8">Front-end Engineer</h2>
-    <p class="text-xl mb-8">I am a front-end engineer focusing on JavaScript, Vue, Sass and semantic HTML. My experience in design and UX helps create quality user friendly websites and software. Lead Engineer at <a href="https://blackfynn.com">Blackfynn.</a></p>
+    <h1 class="text-4xl sm:text-5xl font-bold mb-0">Cameron Baney</h1>
+    <h2 class="text-2xl sm:text-3xl font-semibold mb-8">Front-end Engineer</h2>
+    <p class="text-lg sm:text-xl mb-8">I am a front-end engineer focusing on JavaScript, Vue, Sass and semantic HTML. My experience in design and UX helps create quality user friendly websites and software. Lead Engineer at <a href="https://blackfynn.com">Blackfynn.</a></p>
     <ul class="flex flex-row">
-      <li class="mx-4 first:ml-0 last:mr-0" v-for="item in social" :key="item.link">
-        <a :href="item.link" :title="item.title">
-          <font-awesome-icon class="h-full w-full" :icon="['fab', item.icon]"  size="3x"  />
+      <li class="mx-4 sm:mx-6 first:ml-0 last:mr-0" v-for="item in social" :key="item.link">
+        <a class="h-8 sm:h-12 w-8 sm:w-12 block" :href="item.link" :title="item.title">
+          <font-awesome-icon class="h-full w-full" fixed-width  :icon="['fab', item.icon]" />
         </a>
       </li>
     </ul>
