33f58dcef8b34267436c0080159b8b7e65836221
Cameron Baney <cbaney@gmail.com>
2020-08-09 15:42:28 -0400

    Add OG image

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content: '/favicon.svg'
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
