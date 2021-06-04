49151b74fab4395c09696861fe4208262a2d4348
Cameron Baney <cbaney@gmail.com>
2021-06-04 11:38:22 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="text-4xl sm:text-5xl font-bold mb-0">Cameron Baney</h1>
     <h2 class="text-2xl sm:text-3xl font-semibold mb-8">Front-end Engineer</h2>
-    <p class="text-lg sm:text-xl mb-8">I am a front-end engineer focusing on JavaScript, Vue, Sass and semantic HTML. My experience in design and UX helps create quality user friendly websites and software. Developer on the Messaging team at <a href="https://shopify.engineering/">Shopify.</a></p>
+    <p class="text-lg sm:text-xl mb-8">I am a front-end engineer focusing on JavaScript, Vue, Sass and semantic HTML. My experience in design and UX helps create quality user friendly websites and software. Developer at <a href="https://shopify.engineering/">Shopify.</a></p>
     <ul class="flex flex-row">
       <li class="mx-4 sm:mx-6 first:ml-0 last:mr-0" v-for="item in social" :key="item.link">
         <a class="h-8 sm:h-12 w-8 sm:w-12 block" :href="item.link" :title="item.title">
